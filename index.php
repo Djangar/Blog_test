cd1a710eb50d3a34cf55904598c448f456b51cb6
Tsagan <nonievs@gmail.com>
2013-05-18 22:42:13 +0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<?
-phpinfo();
-echo "Привет!!!"
-?>
+<html>
+  <head>
+    <title><?=$title;?></title>
+  <head>
+  <body>
+    <div id="header">HEADER</div>
+    <div id="content"><?=$content;?></div>
+    <div id="footer">FOOTER</div>
+  </body>
+</html>
